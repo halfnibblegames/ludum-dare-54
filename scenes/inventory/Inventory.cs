e5f0ad46f6280e237214a0f2265f0f738cec49cf
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-09-30 13:39:38 +0200

    👜 Implement inventory

--- scenes/inventory/Inventory.cs
+++ scenes/inventory/Inventory.cs
@@ -3,7 +3,7 @@ using Godot;
 using Array = System.Array;
 
 [Tool]
-public sealed class Inventory : Node
+public sealed class Inventory : Node2D
 {
     private int width = 3;
     private int height = 3;
@@ -34,6 +34,26 @@ public sealed class Inventory : Node
 
     [Export] private Item?[] items = Array.Empty<Item?>();
 
+    public Item? this[int x, int y]
+    {
+        get => items[toIndex(x, y)];
+        set => items[toIndex(x, y)] = value;
+    }
+
+    private int toIndex(int x, int y)
+    {
+        if (x < 0 || x >= width)
+        {
+            throw new ArgumentOutOfRangeException(nameof(x));
+        }
+        if (y < 0 || y >= height)
+        {
+            throw new ArgumentOutOfRangeException(nameof(y));
+        }
+
+        return y * width + x;
+    }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -42,7 +62,6 @@ public sealed class Inventory : Node
 
     private void applyDimensionChange()
     {
-        GD.Print("Apply dimension change");
         items = new Item?[width * height];
         GetTree().CallGroup("slots", "queue_free");
         for (var y = 0; y < height; y++)
@@ -50,26 +69,27 @@ public sealed class Inventory : Node
             for (var x = 0; x < width; x++)
             {
                 var slot = itemSlotScene.Instance<Node2D>();
-                slot.Position = new Vector2((x + 0.5f) * 12, (y + 0.5f) * 12);
+                slot.Position = new Vector2((x + 0.5f) * ItemSlot.Size, (y + 0.5f) * ItemSlot.Size);
+                if (!Engine.EditorHint)
+                {
+                    slot.Connect(
+                        nameof(ItemSlot.ItemDropped),
+                        this,
+                        nameof(onItemDropped),
+                        new Godot.Collections.Array(x, y));
+                }
                 AddChild(slot);
             }
         }
     }
 
-    public Item? this[int x, int y]
+    private void onItemDropped(Item? item, int x, int y)
     {
-        get
+        if (this[x, y] is not null)
         {
-            if (x < 0 || x >= width)
-            {
-                throw new ArgumentOutOfRangeException(nameof(x));
-            }
-            if (y < 0 || y >= height)
-            {
-                throw new ArgumentOutOfRangeException(nameof(y));
-            }
-
-            return items[y * width + x];
+            throw new InvalidOperationException();
         }
+
+        this[x, y] = item;
     }
 }
---
+++ scenes/inventory/ItemSlot.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+public class ItemSlot : Area2D
+{
+    public const int Size = 12;
+
+    [Signal]
+    public delegate void ItemDropped(Item? item);
+
+    [Export] private PackedScene itemScene = null!;
+    private Item? item;
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (Engine.EditorHint) return;
+        if (item is not null) return;
+
+        if (@event is InputEventMouseButton { Pressed: true, ButtonIndex: (int) ButtonList.Left } e)
+        {
+            var d = e.Position - Position;
+            const float r = 0.5f * Size;
+            if (d.x is >= -r and < r && d.y is >= -r and < r)
+            {
+                fillWithItem();
+            }
+        }
+    }
+
+    private void fillWithItem()
+    {
+        item = itemScene.Instance<Item>();
+        AddChild(item);
+        EmitSignal(nameof(ItemDropped), item);
+    }
+}
