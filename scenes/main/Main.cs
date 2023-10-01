08e5dcccbcba82ee4791cb10349860de0318868b
William Barbosa <heytherewill@gmail.com>
2023-10-01 09:36:18 +0200

    🧳 Animate inventory display

--- scenes/main/Main.cs
+++ scenes/main/Main.cs
@@ -1,5 +1,7 @@
 using Godot;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 public sealed class Main : Node
 {
@@ -11,25 +13,51 @@ public sealed class Main : Node
         var dungeon = Dungeon.Make();
 
         var dialogue = GetNode<Dialogue>("Dialogue");
+        dialogue.Visible = true;
+        
+        var sceneAnimationTasks = new[]
+        {
+            ToggleInventory(forceState: false),
+            dialogue
+                .DisplayDialog(dungeon.Monologue.Select(x => new Sentence(Portrait.Player, x)).ToList())
+        };
+
         var inventory = GetNode<Inventory>("Inventory");
-        inventory.Visible = false;
         inventory.Populate(dungeon.StartingItems);
         
-        dialogue.Visible = true;
-        ;
-        await dialogue
-            .DisplayDialog(dungeon.Monologue.Select(x => new Sentence(Portrait.Player, x)).ToList());
-
+        await Task.WhenAll(sceneAnimationTasks);
         dialogue.Visible = false;
         
         dungeonTraverser = new DungeonTraverser(dungeon);
 
         GetNode<Room>("Room").FillRoom(dungeonTraverser.CurrentRoom);
+        await ToggleInventory(forceState: true);
     }
 
     private void onRoomExited()
     {
         dungeonTraverser.MoveForward();
         GetNode<Room>("Room").FillRoom(dungeonTraverser.CurrentRoom);
+    }
+
+    private bool currentInventoryVisibility = true;
+    private async Task ToggleInventory(bool? forceState = null)
+    {
+        var inventory = GetNode<Inventory>("Inventory");
+        var shouldBeVisible = forceState ?? inventory.Position.x < 0;
+        if (shouldBeVisible == currentInventoryVisibility)
+            return;
+
+        var targetX = shouldBeVisible ? 5 : -100;
+        var step = shouldBeVisible ? 1 : -1;
+        do
+        {
+            // TODO: Make this animation less stupid
+            inventory.Position = inventory.Position with { x = inventory.Position.x + step };
+            await Task.Delay(1);
+        } while (Math.Abs(inventory.Position.x - targetX) > 0.01);
+            
+        // TODO(will): Wait for player input
+        await Task.Delay(1000);
     }
 }
